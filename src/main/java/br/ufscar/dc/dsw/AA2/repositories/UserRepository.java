1e9fa6a9017258fbf9086034826139447255c144
vitoriahilgert <vihilgerttomasel@gmail.com>
2025-06-18 22:33:16 -0300

    LOGIN FUNCIONANDOOO

--- src/main/java/br/ufscar/dc/dsw/AA2/repositories/UserRepository.java
+++ src/main/java/br/ufscar/dc/dsw/AA2/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package br.ufscar.dc.dsw.AA2.repositories;
 import br.ufscar.dc.dsw.AA2.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
