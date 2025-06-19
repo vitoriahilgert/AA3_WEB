2835162e4e7dc0ec4237ad2ccec209ef94f09f17
vitoriahilgert <vihilgerttomasel@gmail.com>
2025-06-18 21:40:37 -0300

    feat: adicionando proteção de permissões e home

---
+++ src/main/java/br/ufscar/dc/dsw/AA2/config/DatabaseSeeder.java
@@ -0,0 +1,29 @@
+package br.ufscar.dc.dsw.AA2.config;
+
+import br.ufscar.dc.dsw.AA2.models.User;
+import br.ufscar.dc.dsw.AA2.models.enums.UserRoleEnum;
+import br.ufscar.dc.dsw.AA2.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
+@Configuration
+public class DatabaseSeeder {
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.count() == 0) {
+                User user = new User();
+                user.setEmail("vihilgerttomasel@gmail.com");
+                user.setPassword("password");
+                user.setName("Vitória Admin");
+                user.setRole(UserRoleEnum.ADMIN);
+                userRepository.save(user);
+                System.out.println("Seed executada com sucesso.");
+            }
+        };
+    }
+}
---
+++ src/main/java/br/ufscar/dc/dsw/AA2/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package br.ufscar.dc.dsw.AA2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_TESTER");
+        return roleHierarchy;
+    }
+
+    @Bean
+    public DefaultWebSecurityExpressionHandler expressionHandler() {
+        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy());
+        return expressionHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+//                .requestMatchers("/tester/**").hasRole("TESTER")
+//                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+        )
+            .formLogin((form) -> form
+                    .loginPage("/login")
+                    .permitAll()
+            );
+        return http.build();
+    }
+}
