822d60c3a098741cb4c63594c082b4b3c406fad9
Rodrigo Smith <smithrodrigues08@gmail.com>
2025-07-13 15:07:18 -0300

    feat: refatorações para transformar em uma API REST com validações

--- src/main/java/br/ufscar/dc/dsw/AA2/controllers/UserController.java
+++ src/main/java/br/ufscar/dc/dsw/AA2/controllers/UserController.java
@@ -1,56 +1,70 @@
 package br.ufscar.dc.dsw.AA2.controllers;
 
-import br.ufscar.dc.dsw.AA2.dtos.UserRecordDto;
-import br.ufscar.dc.dsw.AA2.models.User;
+import br.ufscar.dc.dsw.AA2.dtos.user.*;
+import br.ufscar.dc.dsw.AA2.models.enums.UserRoleEnum;
 import br.ufscar.dc.dsw.AA2.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RequestMapping("/api/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
     @Autowired
     private UserService userService;
 
+    @PostMapping
+    public ResponseEntity<GetUserResponseDTO> create(
+            @RequestHeader("Authorization") String token,
+            @RequestBody CreateUserRequestDTO request) {
+        GetUserResponseDTO user = userService.createUser(token, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+    public ResponseEntity<List<GetUserResponseDTO>> getAll(@RequestHeader("Authorization") String token) {
+        List<GetUserResponseDTO> users = userService.getAllUsers(token);
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable UUID id) {
-        User user = userService.getUserById(id);
-        if (user != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(user);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-
-    @PostMapping
-    public ResponseEntity<User> saveUser(@RequestBody UserRecordDto userDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(userDto));
+    public ResponseEntity<GetUserResponseDTO> getById(
+            @RequestHeader("Authorization") String token,
+            @PathVariable UUID id) {
+        GetUserResponseDTO user = userService.getUserById(token, id);
+        return ResponseEntity.ok(user);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable UUID id, @RequestBody UserRecordDto userDto) {
-        User updatedUser = userService.updateUser(id, userDto);
-        if (updatedUser != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    public ResponseEntity<GetUserResponseDTO> update(
+            @RequestHeader("Authorization") String token,
+            @PathVariable UUID id,
+            @RequestBody UpdateUserRequestDTO request) {
+        GetUserResponseDTO updatedUser = userService.updateUser(token, id, request);
+        return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable UUID id) {
-        userService.deleteUser(id);
-        return ResponseEntity.status(HttpStatus.OK).body("Usuário com id " + id + " foi deletado com sucesso.");
+    public ResponseEntity<Void> delete(
+            @RequestHeader("Authorization") String token,
+            @PathVariable UUID id) {
+        userService.deleteUser(token, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/admins")
+    public ResponseEntity<List<GetUserResponseDTO>> getAdmins(@RequestHeader("Authorization") String token) {
+        List<GetUserResponseDTO> admins = userService.getUsersByRole(token, UserRoleEnum.ADMIN);
+        return ResponseEntity.ok(admins);
+    }
+
+    @GetMapping("/testers")
+    public ResponseEntity<List<GetUserResponseDTO>> getTesters(@RequestHeader("Authorization") String token) {
+        List<GetUserResponseDTO> testers = userService.getUsersByRole(token, UserRoleEnum.TESTER);
+        return ResponseEntity.ok(testers);
     }
 }
