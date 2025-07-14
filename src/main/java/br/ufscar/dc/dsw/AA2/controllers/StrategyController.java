b919b983900a3e5b042574f64514fbe4cfdeed6e
pedro.sakai <pedro.sakai@ifood.com.br>
2025-07-14 08:27:41 -0300

    remove /api

--- src/main/java/br/ufscar/dc/dsw/AA2/controllers/StrategyController.java
+++ src/main/java/br/ufscar/dc/dsw/AA2/controllers/StrategyController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/strategies")
+@RequestMapping("/strategies")
 public class StrategyController {
 
     @Autowired
